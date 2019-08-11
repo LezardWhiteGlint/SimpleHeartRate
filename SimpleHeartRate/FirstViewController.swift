d2ec0d43913f38ace83f094621655f41eb3cf53a
LezardWhiteGlint <30715385+LezardWhiteGlint@users.noreply.github.com>
2019-08-11 14:46:26 +0800

    Initial Commit

---
+++ SimpleHeartRate/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SimpleHeartRate
+//
+//  Created by Lezardvaleth on 2019/8/11.
+//  Copyright © 2019 Lezardvaleth. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleHeartRate/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SimpleHeartRate
+//
+//  Created by Lezardvaleth on 2019/8/11.
+//  Copyright © 2019 Lezardvaleth. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
